8ebea49a4365e99ed37b034612eae9322329dfc1
Sudipta Adhikary Joy <dipto5russow@gmail.com>
2023-11-06 22:03:08 +0600

    Add files via upload

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Imagegallery from "../ui/features/imagegallery";
+
+const MainLayout = () => {
+
+    return (
+        <div className="bg-white text-black">
+           <Imagegallery/>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import MainLayout from "@/components/layouts/MainLayout";
+import React from "react";
+
+const Home = () => {
+
+  return (
+    <div >
+      <MainLayout/>
+    </div>
+  );
+};
+
+export default Home;
