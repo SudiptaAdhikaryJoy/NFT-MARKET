fc390ffc0c8b97e185e41d53126b52d20c4e8160
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2023-11-06 22:38:25 +0600

    fix:metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import MainLayout from "@/components/layouts/MainLayout";
 import React from "react";
+import Head from "next/head";
+import metaData from '../../public/meta.json'
 
 const Home = () => {
 
   return (
-    <div >
+    <>
+    <Head>
+      <title>Image Gallery</title>
+        <meta name="description" content={metaData.description} />
+        <meta name="keywords" content={metaData.keywords} />
+        <meta property="og:description" content={metaData.description} />
+        <meta property="og:url" content={metaData.url} />
+        <meta property="og:type" content={metaData.type} />
+        <meta property="og:image" content={metaData.image} />      
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
       <MainLayout/>
-    </div>
+    </>
   );
 };
 
